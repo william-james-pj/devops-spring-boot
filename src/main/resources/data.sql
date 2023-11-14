59c6dee0299b191084300fb9f0f594cfc95330be
William James <52788612+william-james-pj@users.noreply.github.com>
2023-11-14 08:28:48 -0300

    feat: add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `curso` VALUES ("Introdução à Psicologia 3");
+INSERT INTO `curso` VALUES ("Programação em Python para Iniciantes");
+INSERT INTO `curso` VALUES ("Gestão Financeira Pessoal");
+
+INSERT INTO `aluno` VALUES ("Maria da Silva", 1);
+INSERT INTO `aluno` VALUES ("Pedro Santos", 1);
+INSERT INTO `aluno` VALUES ("Ana Pereira", 1);
+
+INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 1, 1);
+INSERT INTO `matricula` VALUES (8.0, false, "FINALIZADO", 2, 2);
+INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 3, 2);
+INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 3, 3);
