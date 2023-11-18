89fd8bde5ea476e6c07d80cb5e2416290aec5931
William James <52788612+william-james-pj@users.noreply.github.com>
2023-11-18 11:37:04 -0300

    fix: h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO `curso` VALUES ("Introdução à Psicologia 3");
-INSERT INTO `curso` VALUES ("Programação em Python para Iniciantes");
-INSERT INTO `curso` VALUES ("Gestão Financeira Pessoal");
 
-INSERT INTO `aluno` VALUES ("Maria da Silva", 1);
-INSERT INTO `aluno` VALUES ("Pedro Santos", 1);
-INSERT INTO `aluno` VALUES ("Ana Pereira", 1);
+INSERT INTO CURSO(nome) VALUES('Introdução à Psicologia 3');
+INSERT INTO CURSO(nome) VALUES('Programação em Python para Iniciantes');
+INSERT INTO CURSO(nome) VALUES('Gestão Financeira Pessoal');
 
-INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 1, 1);
-INSERT INTO `matricula` VALUES (8.0, false, "FINALIZADO", 2, 2);
-INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 3, 2);
-INSERT INTO `matricula` VALUES (0, false, "CURSANDO", 3, 3);
+INSERT INTO ALUNO(nome, qtd_cursos_disponivel) VALUES('Maria da Silva', 1);
+INSERT INTO ALUNO(nome, qtd_cursos_disponivel) VALUES('Pedro Santos', 1);
+INSERT INTO ALUNO(nome, qtd_cursos_disponivel) VALUES('Ana Pereira', 1);
+
+INSERT INTO MATRICULA(nota, pode_fazer_sub, status, curso_id, aluno_ra) VALUES(0, false, 'CURSANDO', 1, 1);
+INSERT INTO MATRICULA(nota, pode_fazer_sub, status, curso_id, aluno_ra) VALUES(8.0, false, 'FINALIZADO', 2, 2);
+INSERT INTO MATRICULA(nota, pode_fazer_sub, status, curso_id, aluno_ra) VALUES(0, false, 'CURSANDO', 3, 2);
