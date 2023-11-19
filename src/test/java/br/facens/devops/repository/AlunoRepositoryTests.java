9f4192b61193e08628d2f3b841261db4c16f95ac
William James <52788612+william-james-pj@users.noreply.github.com>
2023-11-19 16:43:32 -0300

    fix: repository tests

--- src/test/java/br/facens/devops/repository/AlunoRepositoryTests.java
+++ src/test/java/br/facens/devops/repository/AlunoRepositoryTests.java
@@ -6,83 +6,69 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.facens.devops.entity.Aluno;
 
-//@DataJpaTest
-//@ExtendWith(SpringExtension.class)
-//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-//class AlunoRepositoryTests {
-//
-//	@Autowired
-//	AlunoRepository repository;
-//
-//	@BeforeEach
-//    public void setUp() {
-//		repository.save(new Aluno("Joao", 1));
-//		repository.save(new Aluno("Maria", 1));
-//	}
-//
-//    @AfterEach
-//    public void destroy() {
-//    	repository.deleteAll();
-//    }
-//    
-//    @Test
-//    public void testGetAllAlunos() {
-//        List<Aluno> alunos = repository.findAll();
-//        
-//        Assertions.assertThat(alunos.size()).isEqualTo(2);
-//        Assertions.assertThat(alunos.get(0).getRA()).isNotNegative();
-//        Assertions.assertThat(alunos.get(0).getRA()).isGreaterThan(0);
-//        Assertions.assertThat(alunos.get(0).getNome()).isEqualTo("Joao");
-//    }
-//    
-//    @Test
-//    public void testGetInvalidAluno() {
-//        Exception exception = assertThrows(NoSuchElementException.class, () -> {
-//        	repository.findById(15).get();
-//        });
-//        
-//        Assertions.assertThat(exception).isNotNull();
-//        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
-//        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
-//    }
-//    
-//    @Test
-//    public void testGetCreateAluno() {
-//    	Aluno saved = new Aluno("Pedro", 1);
-//    	
-//    	Aluno returned = repository.save(saved);
-//    	
-//        Assertions.assertThat(returned).isNotNull();
-//        Assertions.assertThat(returned.getRA()).isNotNegative();
-//        Assertions.assertThat(returned.getRA()).isGreaterThan(0);
-//        Assertions.assertThat(saved.getNome()).isEqualTo(returned.getNome());
-//    }
-//
-//    @Test
-//    public void testDeleteAluno() {
-//    	Aluno saved = new Aluno("Pedro", 1);
-//        repository.save(saved);
-//        
-//        repository.delete(saved);
-//        Exception exception = assertThrows(NoSuchElementException.class, () -> {
-//        	repository.findById(13).get();
-//        });
-//        
-//        Assertions.assertThat(exception).isNotNull();
-//        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
-//        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
-//    }
-//}
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class AlunoRepositoryTests {
+
+	@Autowired
+	AlunoRepository repository;
+    
+    @Test
+    public void testGetAllAlunos() {
+        List<Aluno> alunos = repository.findAll();
+        
+        Assertions.assertThat(alunos.size()).isEqualTo(3);
+        Assertions.assertThat(alunos.get(0).getRA()).isNotNegative();
+        Assertions.assertThat(alunos.get(0).getRA()).isGreaterThan(0);
+        Assertions.assertThat(alunos.get(0).getNome()).isEqualTo("Maria da Silva");
+    }
+    
+    @Test
+    public void testGetInvalidAluno() {
+        Exception exception = assertThrows(NoSuchElementException.class, () -> {
+        	repository.findById(15).get();
+        });
+        
+        Assertions.assertThat(exception).isNotNull();
+        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+    
+    @Test
+    public void testGetCreateAluno() {
+    	Aluno saved = new Aluno("Pedro", 1);
+    	
+    	Aluno returned = repository.save(saved);
+    	
+        Assertions.assertThat(returned).isNotNull();
+        Assertions.assertThat(returned.getRA()).isNotNegative();
+        Assertions.assertThat(returned.getRA()).isGreaterThan(0);
+        Assertions.assertThat(saved.getNome()).isEqualTo(returned.getNome());
+    }
+
+    @Test
+    public void testDeleteAluno() {
+    	Aluno saved = new Aluno("Pedro", 1);
+        repository.save(saved);
+        
+        repository.delete(saved);
+        Exception exception = assertThrows(NoSuchElementException.class, () -> {
+        	repository.findById(13).get();
+        });
+        
+        Assertions.assertThat(exception).isNotNull();
+        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+}
--- src/test/java/br/facens/devops/repository/CursoRepositoryTests.java
+++ src/test/java/br/facens/devops/repository/CursoRepositoryTests.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,70 +16,59 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.facens.devops.entity.Curso;
 
-//@DataJpaTest
-//@ExtendWith(SpringExtension.class)
-//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-//class CursoRepositoryTests {
-//
-//	@Autowired
-//	CursoRepository repository;
-//
-//	@BeforeEach
-//    public void setUp() {
-//		repository.save(new Curso("Matematica"));
-//		repository.save(new Curso("Historia"));
-//	}
-//
-//    @AfterEach
-//    public void destroy() {
-//    	repository.deleteAll();
-//    }
-//    
-//    @Test
-//    public void testGetAllCursos() {
-//        List<Curso> cursos = repository.findAll();
-//        
-//        Assertions.assertThat(cursos.size()).isEqualTo(2);
-//        Assertions.assertThat(cursos.get(0).getId()).isNotNegative();
-//        Assertions.assertThat(cursos.get(0).getId()).isGreaterThan(0);
-//        Assertions.assertThat(cursos.get(0).getNome()).isEqualTo("Matematica");
-//    }
-//    
-//    @Test
-//    public void testGetInvalidCurso() {
-//        Exception exception = assertThrows(NoSuchElementException.class, () -> {
-//        	repository.findById(15).get();
-//        });
-//        
-//        Assertions.assertThat(exception).isNotNull();
-//        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
-//        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
-//    }
-//    
-//    @Test
-//    public void testGetCreateCurso() {
-//    	Curso saved = new Curso("Calculo");
-//    	
-//    	Curso returned = repository.save(saved);
-//    	
-//        Assertions.assertThat(returned).isNotNull();
-//        Assertions.assertThat(returned.getId()).isNotNegative();
-//        Assertions.assertThat(returned.getId()).isGreaterThan(0);
-//        Assertions.assertThat(saved.getNome()).isEqualTo(returned.getNome());
-//    }
-//
-//    @Test
-//    public void testDeleteCurso() {
-//    	Curso saved = new Curso("Calculo");
-//        repository.save(saved);
-//        
-//        repository.delete(saved);
-//        Exception exception = assertThrows(NoSuchElementException.class, () -> {
-//        	repository.findById(13).get();
-//        });
-//        
-//        Assertions.assertThat(exception).isNotNull();
-//        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
-//        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
-//    }
-//}
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class CursoRepositoryTests {
+
+	@Autowired
+	CursoRepository repository;
+    
+    @Test
+    public void testGetAllCursos() {
+        List<Curso> cursos = repository.findAll();
+        
+        Assertions.assertThat(cursos.size()).isEqualTo(3);
+        Assertions.assertThat(cursos.get(0).getId()).isNotNegative();
+        Assertions.assertThat(cursos.get(0).getId()).isGreaterThan(0);
+        Assertions.assertThat(cursos.get(0).getNome()).isEqualTo("Introdução à Psicologia 3");
+    }
+    
+    @Test
+    public void testGetInvalidCurso() {
+        Exception exception = assertThrows(NoSuchElementException.class, () -> {
+        	repository.findById(15).get();
+        });
+        
+        Assertions.assertThat(exception).isNotNull();
+        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+    
+    @Test
+    public void testGetCreateCurso() {
+    	Curso saved = new Curso("Calculo");
+    	
+    	Curso returned = repository.save(saved);
+    	
+        Assertions.assertThat(returned).isNotNull();
+        Assertions.assertThat(returned.getId()).isNotNegative();
+        Assertions.assertThat(returned.getId()).isGreaterThan(0);
+        Assertions.assertThat(saved.getNome()).isEqualTo(returned.getNome());
+    }
+
+    @Test
+    public void testDeleteCurso() {
+    	Curso saved = new Curso("Calculo");
+        repository.save(saved);
+        
+        repository.delete(saved);
+        Exception exception = assertThrows(NoSuchElementException.class, () -> {
+        	repository.findById(13).get();
+        });
+        
+        Assertions.assertThat(exception).isNotNull();
+        Assertions.assertThat(exception.getClass()).isEqualTo(NoSuchElementException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+}
